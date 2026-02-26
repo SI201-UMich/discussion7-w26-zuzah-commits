561c6d38c992b5465c1e5a7db153de6821016c1a
Zuza Harris <zuzaharris@Zuzas-MacBook-Air-2.local>
2026-02-26 00:26:00 -0500

    task 1

--- discussion7.py
+++ discussion7.py
@@ -31,7 +31,17 @@ def load_listings(f):
     full_path = os.path.join(base_path, f)
 
     # TODO: Read the CSV using csv.reader and convert it to a list a dictionaries
-    pass
+    listings = []
+    with open(full_path, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader:
+            listing_dict = {}
+            for i in range(len(header)):
+                listing_dict[header[i]] = row[i]
+            listings.append(listing_dict)
+    return listings
+        
 
 ###############################################################################
 ##### TASK 2: CALCULATION FUNCTION (single calculation)
